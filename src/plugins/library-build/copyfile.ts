07f5df95f591999a08aca806eca368913e44e8b6
蒲伟 <wb-pw365304@antfin.com>
2018-12-18 01:25:19 +0800

    feat: support singular component folder

--- src/plugins/library-build/copyfile.ts
+++ src/plugins/library-build/copyfile.ts
@@ -3,13 +3,16 @@ import fse from 'fs-extra';
 import glob from 'glob';
 
 export default class {
-  public cwd: string;
-  constructor() {
+  private cwd: string;
+  private baseFolder: string;
+
+  constructor(baseFolder) {
     this.cwd = process.cwd();
+    this.baseFolder = baseFolder;
   }
 
   public async run() {
-    const from = path.join(this.cwd, './src/components');
+    const from = this.baseFolder;
     return await Promise.all([
       this.copyTypescript(from, path.join(this.cwd, './lib')),
       this.copyTypescript(from, path.join(this.cwd, './es')),
@@ -19,9 +22,7 @@ export default class {
   private copyTypescript(from: string, to: string) {
     const files = glob.sync('**/*.d.ts', { cwd: from });
     const cmds = files.map(file => fse.copy(path.resolve(from, file), path.resolve(to, file)));
-    cmds.push(
-      fse.copy(path.join(this.cwd, './src/components/index.js'), path.join(to, 'index.d.ts'))
-    );
+    cmds.push(fse.copy(path.join(this.baseFolder, 'index.js'), path.join(to, 'index.d.ts')));
     return Promise.all(cmds);
   }
 }
--- src/plugins/library-build/index.ts
+++ src/plugins/library-build/index.ts
@@ -8,17 +8,25 @@ export default class {
   public babel: string;
   public babelRc: string;
   public hasIndex: boolean;
+  public baseFolder: string;
 
   constructor() {
     this.babel = resolveBin('@babel/cli', { executable: 'babel' });
     this.babelRc = join(__dirname, 'babel.config');
-    this.hasIndex = fs.existsSync(join(process.cwd(), 'src/components/index.js'));
+    this.baseFolder = join(process.cwd(), 'src/component');
+    if (!fs.existsSync(this.baseFolder)) {
+      this.baseFolder = join(process.cwd(), 'src/components');
+    }
+    if (!fs.existsSync(this.baseFolder)) {
+      throw new Error('components folder not found!');
+    }
+    this.hasIndex = fs.existsSync(join(this.baseFolder, 'index.js'));
   }
 
   public async build() {
     this.beforeBuild();
     await Promise.all([this.buildEs5(), this.buildEs6()]);
-    await new Copyfile().run();
+    await new Copyfile(this.baseFolder).run();
     this.aferBuild();
   }
 
@@ -28,7 +36,7 @@ export default class {
     fork(rimraf, ['./es']);
     if (!this.hasIndex) {
       const createIndex = resolveBin('create-index');
-      fork(createIndex, ['./src/components']);
+      fork(createIndex, [this.baseFolder]);
     }
   }
 
@@ -36,7 +44,7 @@ export default class {
     return new Promise(resolve => {
       const child = fork(
         this.babel,
-        ['--config-file', this.babelRc, './src/components', '--out-dir', './lib'],
+        ['--config-file', this.babelRc, this.baseFolder, '--out-dir', './lib'],
         {
           env: {
             BABEL_ENV: 'es5',
@@ -53,7 +61,7 @@ export default class {
     return new Promise(resolve => {
       const child = fork(
         this.babel,
-        ['--config-file', this.babelRc, './src/components', '--out-dir', './es'],
+        ['--config-file', this.babelRc, this.baseFolder, '--out-dir', './es'],
         {
           env: {
             BABEL_ENV: 'es6',
@@ -72,7 +80,7 @@ export default class {
   private aferBuild() {
     // 如果原本没有 index, 需要删除.
     if (!this.hasIndex) {
-      fs.unlinkSync('./src/components/index.js');
+      fs.unlinkSync(join(this.baseFolder, 'index.js'));
     }
     // tslint:disable-next-line
     console.log('build umi library done!');
