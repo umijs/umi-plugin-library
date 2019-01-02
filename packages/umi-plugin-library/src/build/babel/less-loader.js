87dc895e5fa7aa7da3a65176a2341aaada2f4c6d
clock157 <clock157@163.com>
2019-01-02 12:27:48 +0800

    fix: less import use double quote not budle.

--- packages/umi-plugin-library/src/build/babel/less-loader.js
+++ packages/umi-plugin-library/src/build/babel/less-loader.js
@@ -11,12 +11,15 @@ const debug = require('debug')('umi-plugin-library');
 module.exports = function processLess(data, filename) {
   let result;
   // less-plugin-npm-import 与 syncImport 冲突, 手工改为相对路径再解析
+  const nodeModulesPath = path.relative(
+    path.dirname(filename),
+    path.join(process.cwd(), 'node_modules')
+  );
   if (data.includes(`@import '~`)) {
-    const nodeModulesPath = path.relative(
-      path.dirname(filename),
-      path.join(process.cwd(), 'node_modules')
-    );
     data = data.replace(`@import '~`, `@import '${nodeModulesPath}/`);
+  }
+  if (data.includes(`@import "~`)) {
+    data = data.replace(`@import "~`, `@import "${nodeModulesPath}/`);
   }
 
   less.render(
